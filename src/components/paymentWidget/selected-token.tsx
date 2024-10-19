83b28b8708f79353e4b2f9a3f4123c0ff934e863
henry-hein <henryhein.hein@gmail.com>
2024-10-18 20:20:06 -0700

    add selected token chains, list

---
+++ src/components/paymentWidget/selected-token.tsx
@@ -0,0 +1,31 @@
+import usePaymentWidget from "@/hooks/use-payment-widget";
+import { Button } from "@nextui-org/button";
+import { FaCaretDown } from "react-icons/fa6";
+
+const SelectedToken = ({ onOpen }: { onOpen: () => void }) => {
+  const { selectedToken } = usePaymentWidget();
+  return (
+    <Button
+      className="flex-none gap-0 p-0 pt-2"
+      data-hover="false"
+      variant="light"
+      onClick={onOpen}
+    >
+      {Object.keys(selectedToken).length ?
+        <>
+          <img src={selectedToken.logoURI} height='24' width='24' />
+          <span className="pl-1 text-l">
+            <b>{selectedToken.symbol}</b>
+          </span>
+        </>
+        :
+        <span className='hover:underline underline-offset-4'>Select token</span>
+      }
+      <span className="pl-1 text-foreground-600">
+        <FaCaretDown size="12" />
+      </span>
+    </Button>
+  );
+}
+
+export default SelectedToken;
