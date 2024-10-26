617853e4a4fd5103b694c0c958c2a100bb18f77e
hasanm95 <hsncool10@gmail.com>
2024-10-27 01:19:48 +0800

    updated public clients

--- src/config/viemPublicClient.ts
+++ src/config/viemPublicClient.ts
@@ -1,4 +1,4 @@
-import { createPublicClient, http, PublicClientConfig, Transport } from 'viem';
+import { createPublicClient, http, PublicClientConfig, Transport } from "viem";
 import {
   mainnet,
   arbitrum,
@@ -7,63 +7,70 @@ import {
   bsc,
   optimism,
   polygon,
-  fantom,
-  aurora,
-  klaytn,
   optimismSepolia,
   arbitrumSepolia,
-  auroraTestnet,
   baseSepolia,
-  klaytnBaobab,
-  bscTestnet
-} from 'wagmi/chains';
+  bscTestnet,
+} from "wagmi/chains";
 
 const config = {
-  chains: [mainnet, arbitrum, avalanche, aurora, base, bsc, fantom, klaytn, optimism, polygon, 
-    optimismSepolia, arbitrumSepolia, klaytnBaobab, auroraTestnet, baseSepolia, bscTestnet],
+  chains: [
+    mainnet,
+    arbitrum,
+    avalanche,
+    base,
+    bsc,
+    optimism,
+    polygon,
+    optimismSepolia,
+    arbitrumSepolia,
+    baseSepolia,
+    bscTestnet,
+  ],
   transports: {
     [mainnet.id]: http(),
     [arbitrum.id]: http(),
     [avalanche.id]: http(),
-    [aurora.id]: http(),
     [base.id]: http(),
     [bsc.id]: http(),
-    [fantom.id]: http(),
-    [klaytn.id]: http(),
     [optimism.id]: http(),
     [polygon.id]: http(),
 
     //testnets
     [optimismSepolia.id]: http(),
     [arbitrumSepolia.id]: http(),
-    [auroraTestnet.id]: http(),
     [baseSepolia.id]: http(),
-    [klaytnBaobab.id]: http(),
-    [bscTestnet.id]: http()
+    [bscTestnet.id]: http(),
   },
 };
 
-
-const clientCache: { [chainId: number]: ReturnType<typeof createPublicClient> } = {};
+const clientCache: {
+  [chainId: number]: ReturnType<typeof createPublicClient>;
+} = {};
 
 const getPublicClient = (chainId: number) => {
   if (clientCache[chainId]) {
-    return clientCache[chainId]; 
+    return clientCache[chainId];
   }
 
-  const chainConfigId: PublicClientConfig['chain']  = config.chains.find(chain => chain.id === chainId);
-  if (!chainConfigId) throw new Error('Chain not supported');
+  const chainConfigId: PublicClientConfig["chain"] = config.chains.find(
+    (chain) => chain.id === chainId,
+  );
 
-  const transport: PublicClientConfig['transport'] = config.transports[chainId as keyof Transport];
+  if (!chainConfigId) throw new Error("Chain not supported");
+
+  const transport: PublicClientConfig["transport"] =
+    config.transports[chainId as keyof Transport];
   const publicClient = createPublicClient({
     chain: chainConfigId,
     transport,
     batch: {
-      multicall: true, 
+      multicall: true,
     },
   });
 
   clientCache[chainId] = publicClient;
+
   return publicClient;
 };
 
