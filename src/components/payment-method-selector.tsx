98c8d69fe529412fbf929e6df06c4fd74b5a2a19
hasanm95 <hsncool10@gmail.com>
2024-10-27 01:18:06 +0800

    updated chain tokens

--- src/components/payment-method-selector.tsx
+++ src/components/payment-method-selector.tsx
@@ -10,17 +10,17 @@ import { Token } from "@/providers/paymentWidget/paymentWidgetContext";
 
 const chainsToTokensMapping: Record<number, Token[]> = {
   1: [
-    {
-      id: 1,
-      chainId: 1,
-      chainName: "Ethereum",
-      symbol: "USDC",
-      name: "USD Coin",
-      address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
-      decimals: 6,
-      logoURI:
-        "https://tokens.1inch.io/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
-    },
+    // {
+    //   id: 1,
+    //   chainId: 1,
+    //   chainName: "Ethereum",
+    //   symbol: "USDC",
+    //   name: "USD Coin",
+    //   address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    //   decimals: 6,
+    //   logoURI:
+    //     "https://tokens.1inch.io/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
+    // },
     {
       id: 2,
       chainId: 1,
@@ -34,17 +34,17 @@ const chainsToTokensMapping: Record<number, Token[]> = {
     },
   ],
   10: [
-    {
-      id: 1,
-      chainId: 10,
-      chainName: "Optimism",
-      symbol: "USDC",
-      name: "USD Coin",
-      address: "0x0b2c639c533813f4aa9d7837caf62653d097ff85",
-      decimals: 6,
-      logoURI:
-        "https://tokens.1inch.io/0x0b2c639c533813f4aa9d7837caf62653d097ff85.png",
-    },
+    // {
+    //   id: 1,
+    //   chainId: 10,
+    //   chainName: "Optimism",
+    //   symbol: "USDC",
+    //   name: "USD Coin",
+    //   address: "0x0b2c639c533813f4aa9d7837caf62653d097ff85",
+    //   decimals: 6,
+    //   logoURI:
+    //     "https://tokens.1inch.io/0x0b2c639c533813f4aa9d7837caf62653d097ff85.png",
+    // },
     {
       id: 2,
       chainId: 10,
@@ -58,17 +58,17 @@ const chainsToTokensMapping: Record<number, Token[]> = {
     },
   ],
   56: [
-    {
-      id: 1,
-      chainId: 56,
-      chainName: "BNB Smart Chain",
-      symbol: "USDC",
-      name: "USD Coin",
-      address: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
-      decimals: 18,
-      logoURI:
-        "https://tokens.1inch.io/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
-    },
+    // {
+    //   id: 1,
+    //   chainId: 56,
+    //   chainName: "BNB Smart Chain",
+    //   symbol: "USDC",
+    //   name: "USD Coin",
+    //   address: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+    //   decimals: 18,
+    //   logoURI:
+    //     "https://tokens.1inch.io/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
+    // },
     {
       id: 2,
       chainId: 56,
@@ -93,27 +93,38 @@ const chainsToTokensMapping: Record<number, Token[]> = {
       logoURI:
         "https://tokens.1inch.io/0xdac17f958d2ee523a2206206994597c13d831ec7.png",
     },
-    {
-      id: 2,
-      chainName: "Polygon",
-      chainId: 137,
-      symbol: "USDC",
-      name: "USD Coin",
-      address: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
-      decimals: 6,
-      logoURI:
-        "https://tokens.1inch.io/0x3c499c542cef5e3811e1192ce70d8cc03d5c3359.png",
-    },
+    // {
+    //   id: 2,
+    //   chainName: "Polygon",
+    //   chainId: 137,
+    //   symbol: "USDC",
+    //   name: "USD Coin",
+    //   address: "0x3c499c542cef5e3811e1192ce70d8cc03d5c3359",
+    //   decimals: 6,
+    //   logoURI:
+    //     "https://tokens.1inch.io/0x3c499c542cef5e3811e1192ce70d8cc03d5c3359.png",
+    // },
   ],
   8453: [
+    // {
+    //   id: 1,
+    //   name: "USD Coin",
+    //   symbol: "USDC",
+    //   address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+    //   decimals: 6,
+    //   logoURI:
+    //     "https://assets.coingecko.com/coins/images/6319/small/usdc.png?1696506694",
+    //   chainId: 8453,
+    //   chainName: "Base",
+    // },
     {
-      id: 1,
-      name: "USD Coin",
-      symbol: "USDC",
-      address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+      id: 2,
+      name: "Tether USD",
+      symbol: "USDT",
+      address: "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2",
       decimals: 6,
       logoURI:
-        "https://assets.coingecko.com/coins/images/6319/small/usdc.png?1696506694",
+        "https://tokens.1inch.io/0xdac17f958d2ee523a2206206994597c13d831ec7.png",
       chainId: 8453,
       chainName: "Base",
     },
@@ -130,30 +141,30 @@ const chainsToTokensMapping: Record<number, Token[]> = {
       logoURI:
         "https://tokens.1inch.io/0xdac17f958d2ee523a2206206994597c13d831ec7.png",
     },
-    {
-      id: 2,
-      chainId: 42161,
-      chainName: "Arbitrum",
-      symbol: "USDC",
-      name: "USD Coin",
-      address: "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
-      decimals: 6,
-      logoURI:
-        "https://tokens.1inch.io/0xaf88d065e77c8cc2239327c5edb3a432268e5831.png",
-    },
+    // {
+    //   id: 2,
+    //   chainId: 42161,
+    //   chainName: "Arbitrum",
+    //   symbol: "USDC",
+    //   name: "USD Coin",
+    //   address: "0xaf88d065e77c8cc2239327c5edb3a432268e5831",
+    //   decimals: 6,
+    //   logoURI:
+    //     "https://tokens.1inch.io/0xaf88d065e77c8cc2239327c5edb3a432268e5831.png",
+    // },
   ],
   43114: [
-    {
-      id: 1,
-      chainName: "Avalanche",
-      chainId: 43114,
-      symbol: "USDC",
-      name: "USD Coin",
-      address: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e",
-      decimals: 6,
-      logoURI:
-        "https://tokens.1inch.io/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
-    },
+    // {
+    //   id: 1,
+    //   chainName: "Avalanche",
+    //   chainId: 43114,
+    //   symbol: "USDC",
+    //   name: "USD Coin",
+    //   address: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e",
+    //   decimals: 6,
+    //   logoURI:
+    //     "https://tokens.1inch.io/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48.png",
+    // },
     {
       id: 2,
       chainName: "Avalanche",
@@ -166,19 +177,19 @@ const chainsToTokensMapping: Record<number, Token[]> = {
         "https://tokens.1inch.io/0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7.png",
     },
   ],
-  42220: [
-    {
-      id: 1,
-      decimals: 6,
-      address: "0x48065fbbe25f71c9282ddf5e1cd6d6a887483d5e",
-      name: "Tether USD",
-      symbol: "USD₮",
-      chainName: "Celo",
-      chainId: 42220,
-      logoURI:
-        "https://assets.coingecko.com/coins/images/325/standard/Tether.png?1696501661",
-    },
-  ],
+  // 42220: [
+  //   {
+  //     id: 1,
+  //     decimals: 6,
+  //     address: "0x48065fbbe25f71c9282ddf5e1cd6d6a887483d5e",
+  //     name: "Tether USD",
+  //     symbol: "USD₮",
+  //     chainName: "Celo",
+  //     chainId: 42220,
+  //     logoURI:
+  //       "https://assets.coingecko.com/coins/images/325/standard/Tether.png?1696501661",
+  //   },
+  // ],
 };
 //TODO: connect to back end api for tokens and chains
 const PaymentMethodSelector = () => {
